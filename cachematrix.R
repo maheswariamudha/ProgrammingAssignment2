36b03416bf63e177093faa06c19f31f10b5f39ba
Maheswari <maheswariamudha@gmail.com>
2015-10-24 22:35:06 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -38,4 +38,27 @@ cacheSolve <- function(x, ...) {
         x$setInverse(inv)
         inv
 }
- 
+
+## Sample test result
+
+##> source("ProgrammingAssignment2/cachematrix.R")
+##> my_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+##> my_matrix$get()
+ ##    [,1] [,2]
+##[1,]    1    3
+##[2,]    2    4
+##> my_matrix$getInverse()
+##NULL
+##> cacheSolve(my_matrix)
+   ##  [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
+##> cacheSolve(my_matrix)
+##getting cached data
+##     [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
+##> my_matrix$getInverse()
+##     [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5 
